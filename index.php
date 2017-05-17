72976494d6f32918d5789322eedb5250518d389e
Micheline Bourque <mbourque@mail.greenriver.edu>
2017-05-16 22:50:19 -0500

    added index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+    //Require the autoload file
+    require_once('vendor/autoload.php');
+
+    session_start();
+    
+    //Create an instance of the Base class
+    $f3 = Base::instance();
+    
+
+    
+    //Define a default route
+    $f3->route('GET /',
+            function() {
+        
+            $view = new View;
+            echo $view->render('pages/home.html');         
+            });
+    
+        //Run fat free
+    $f3->run();
