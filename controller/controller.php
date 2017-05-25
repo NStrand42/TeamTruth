66b74e4b391a252d47ec1c7f93604036169ac9e7
Micheline Bourque <mbourque@mail.greenriver.edu>
2017-05-24 22:04:38 -0500

    added functions to the controller

---
+++ controller/controller.php
@@ -0,0 +1,59 @@
+<?php
+
+class Controller
+{
+    private $sess;
+    
+    function __construct() {
+       $this->sess = new Questionsdb;
+    }
+    
+    /**
+     *This method checks is the user is logged in
+     *
+     *@param $f3 variables
+     */
+    function checkLoggedIn($f3) {
+        if(isset($_SESSION['username']))
+           {
+               $f3->set('usernameCheck', $_SESSION['username']);
+           }
+    }
+    
+    /**
+     *This method renders the home page
+     *
+     *@param $f3 variables
+     */
+    function renderHome($f3) {
+        echo Template::instance()->render('view/homepage.html');  
+    }
+    
+    /**
+     *This method renders the submissions page
+     *
+     *@param $f3 variables
+     */
+    function renderSubmissions($f3) {
+        echo Template::instance()->render('view/submissions.html');
+    }
+    
+    /**
+     *This method renders the hot to page
+     *
+     *@param $f3 variables
+     */    
+    function renderHowToPage($f3) {
+        echo Template::instance()->render('view/howtopage.html');
+    }
+    
+    /**
+     *This method renders the create user page
+     *
+     *@param $f3 variables
+     */    
+    function renderCreateUser($f3) {
+        echo Template::instance()->render('view/createuser.php');
+    }
+    
+}
