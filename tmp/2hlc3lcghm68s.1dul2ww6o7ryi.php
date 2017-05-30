730ba000c879194730adbc2d3049e4afc57991d4
Micheline Bourque <mbourque@mail.greenriver.edu>
2017-05-29 23:22:18 -0500

    fixed homepage

--- tmp/2hlc3lcghm68s.1dul2ww6o7ryi.php
+++ tmp/2hlc3lcghm68s.1dul2ww6o7ryi.php
@@ -2,7 +2,8 @@
     <div class="container">
         <div class="row">
             <!--Left side-->
-            <div class="col-sm=3">
+            <div class="col-sm-3">
+                
                 <div class="row">
                     <div class="col-sm-6">
                         <ul>
@@ -26,23 +27,116 @@
                             <li>Random</li>
                             <li>My personal</li>
                             <li>Create you own</li>
-                        </ul>
-                        
+                        </ul>                       
                     </div>
                 </div>
+                
                 <div class="row">
+                    <div class="col-sm-12">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <h1>Tutorial</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class ="col-sm-12">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <h3>Welcome John Doe!</h3>
+                                <hr />
+                                <h3>Score : ###</h3>
+                                <hr />
+                                <h3>Submissions: ###</h3>    
+                            </div>
+                        </div>     
+                    </div>
+                </div>
+                
+            </div>
+            <!--right side-->
+            <div class="col-sm-9">
+                
+                <div class="row">
+                    <div class="col-sm-6">
+                        
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                       <h1>Player 1: ##</h1> 
+                                    </div>                               
+                                </div>     
+                            </div>
+                        </div>
+                        
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading"><h1>Truth</h1></div>
+                                    <div class="panel-body"><h6>Name your favorite food</h6></div>
+                                    <div class="panel-heading"><h1>Chicken -1 Score</h1></div>
+                                    
+                                    <div class="panel-heading">
+                                        <div class="row">
+                                            <div class="col-sm-6">                                         
+                                                <h1>Like</h1>                                             
+                                            </div>
+                                            <div class="col-sm-6">                                           
+                                                <h1>Dislike</h1>                                             
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        
+                    </div>
+                    
+                    <div class="col-sm-6">
+                        
+                        <div class="row">
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                   <h1>Player 2: ###</h1> 
+                                </div>                               
+                            </div>
+                        </div>
+                        
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading"><h1>Dare</h1></div>
+                                    <div class="panel-body">Do a handstand</div>
+                                    <div class="panel-heading"><h1>Chicken -4 Score</h1></div>
+                                    
+                                    <div class="panel-heading">
+                                        <div class="row">
+                                            <div class="col-sm-6">                                         
+                                                <h1>Like</h1>                                             
+                                            </div>
+                                            <div class="col-sm-6">                                           
+                                                <h1>Dislike</h1>                                             
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>                                
+                            </div>
+                        </div>
+                        
+                    </div>
+                    
+                    <div class="col-sm-12">
+                        
+                    </div>
+                    
                     <div class="col-sm-12">
                         
                     </div>
                 </div>
-                <div class="row">
-                    <div class ="col-sm-12">
-                        
-                    </div>
-                </div>       
-            </div>
-            <!--right side-->
-            <div class="col-sm-9">
                 
             </div>
         </div>
