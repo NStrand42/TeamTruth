69b0d9a16c4629e0fe60b623bdbc558ee4f3441f
Micheline Bourque <mbourque@mail.greenriver.edu>
2017-05-29 22:56:47 -0500

    added bootstrap to homepage

--- tmp/2hlc3lcghm68s.1dul2ww6o7ryi.php
+++ tmp/2hlc3lcghm68s.1dul2ww6o7ryi.php
@@ -1,1 +1,50 @@
-<h3>Homepage</h3>
+<body>
+    <div class="container">
+        <div class="row">
+            <!--Left side-->
+            <div class="col-sm=3">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <ul>
+                            <li>Truth</li>
+                            <li>Relationships</li>
+                            <li>Favorites</li>
+                            <li>Teen</li>
+                            <li>Kids</li>
+                            <li>Random</li>
+                            <li>My personal</li>
+                            <li>Create you own</li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6">
+                        <ul>
+                            <li>Dare</li>
+                            <li>Relationships</li>
+                            <li>Favorites</li>
+                            <li>Teen</li>
+                            <li>Kids</li>
+                            <li>Random</li>
+                            <li>My personal</li>
+                            <li>Create you own</li>
+                        </ul>
+                        
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        
+                    </div>
+                </div>
+                <div class="row">
+                    <div class ="col-sm-12">
+                        
+                    </div>
+                </div>       
+            </div>
+            <!--right side-->
+            <div class="col-sm-9">
+                
+            </div>
+        </div>
+    </div>
+</body>
